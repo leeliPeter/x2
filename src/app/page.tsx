63af14e87b96e67c278c6704896f996cb912388f
Lei Ieong Tam <leeli.petertam@gmail.com>
2024-12-24 14:04:23 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <div className="text-3xl text-primary">
+      <Button>Click me</Button>
+    </div>
+  );
+}
